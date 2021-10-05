519460645cdc6f7d4e6e83d5e381c77e879d5642
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-04 22:01:54 -0600

    added validation to task list so you can not add the same name, empty string, null or input with value enter something valid

--- js/app.js
+++ js/app.js
@@ -1,13 +1,51 @@
 const listContainer = document.querySelector('.task-list');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = ['name', 'todo'];
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'todo'
+}];
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    function invalidInput() {
+        return newListInput.value = null; 
+    }
+    const listName = newListInput.value;
+    if (listName === null || listName === '' || listName === 'Enter something valid') {
+        newListInput.value = 'Enter something valid';
+        setTimeout(invalidInput, 1000);
+        return
+    };
+    for(let list of lists) {
+        if (listName.toLowerCase() === list.name.toLowerCase()) {
+            newListInput.value = 'Enter something valid';
+            setTimeout(invalidInput, 1000);
+            return
+        };
+    };
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    render();
+});
+
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: []}
+}
 
 function render() {
     listContainer.innerHTML = '';
     lists.forEach(list => {
         const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
         listElement.classList.add('list-name');
-        listElement.innerText = list;
+        listElement.innerText = list.name;
         listContainer.append(listElement);
     });
 };
