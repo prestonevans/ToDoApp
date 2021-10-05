7f80f026e04267bc929afdcb3889b8de65eb5149
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-04 21:08:19 -0600

    added render function for task-list

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const listContainer = document.querySelector('.task-list');
+
+let lists = ['name', 'todo'];
+
+function render() {
+    listContainer.innerHTML = '';
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.classList.add('list-name');
+        listElement.innerText = list;
+        listContainer.append(listElement);
+    });
+};
+render();
