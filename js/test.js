f6d0edd61c91e158b7421514eb3913949ad80ba4
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-14 20:40:30 -0600

    added name exist if array has same name

---
+++ js/test.js
@@ -0,0 +1,10 @@
+function gemstones(arr) {
+    // Write your code here
+    for (let i = 0; i < arr[i].length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            console.log(arr[i][j]);
+        }
+    }
+}
+
+gemstones(['hello','preston'])
