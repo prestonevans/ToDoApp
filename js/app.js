5235fc2079093d3e7bfde0a7ea1f2112e0e36060
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-10-15 08:27:16 -0600

    added active list functionality

--- js/app.js
+++ js/app.js
@@ -2,16 +2,20 @@ const listContainer = document.querySelector('.task-list');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = [
-	{
-		id: 1,
-		name: 'name'
-	},
-	{
-		id: 2,
-		name: 'todo'
+const LocalStorageList = 'list';
+const LocalStorageActiveList = 'active.list';
+
+let lists = JSON.parse(localStorage.getItem(LocalStorageList)) || [];
+let SelectedList = localStorage.getItem(LocalStorageActiveList);
+
+listContainer.addEventListener('click', (e) => {
+	if (e.target.tagName.toLowerCase() === 'li') {
+		e.target.classList.add('active-list');
+		SelectedList = e.target.dataset.listId;
 	}
-];
+	render();
+	save();
+});
 
 newListForm.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -19,7 +23,12 @@ newListForm.addEventListener('submit', (e) => {
 		return (newListInput.value = null);
 	}
 	const listName = newListInput.value.trim();
-	if (listName === null || listName === '' || listName === 'Enter something valid') {
+	if (
+		listName === null ||
+		listName === '' ||
+		listName === 'Enter something valid' ||
+		listName === 'Name already exists'
+	) {
 		newListInput.value = 'Enter something valid';
 		setTimeout(invalidInput, 1000);
 		return;
@@ -35,19 +44,31 @@ newListForm.addEventListener('submit', (e) => {
 	newListInput.value = null;
 	lists.push(list);
 	render();
+	save();
 });
 
 function createList(name) {
 	return { id: Date.now().toString(), name: name, tasks: [] };
 }
 
+function save() {
+	localStorage.setItem(LocalStorageList, JSON.stringify(lists));
+	localStorage.setItem(LocalStorageActiveList, SelectedList);
+}
+
 function render() {
 	listContainer.innerHTML = '';
-	lists.forEach((list) => {
+	lists.forEach((list, i) => {
 		const listElement = document.createElement('li');
 		listElement.dataset.listId = list.id;
 		listElement.classList.add('list-name');
 		listElement.innerText = list.name;
+		if (!SelectedList) {
+			SelectedList = lists[0].id;
+		}
+		if (list.id === SelectedList) {
+			listElement.classList.add('active-list');
+		}
 		listContainer.append(listElement);
 	});
 }
